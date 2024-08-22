96700f185fc722fcf2e01b25ea0e2704f312cd06
Govindkv <Govind26663355@gmail.com>
2024-08-22 11:27:01 +0530

    CI/CD Added

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 import setuptools
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
+with open("README.md","r", encoding="utf-8") as f:
     long_description = f.read()
+
 
 __version__ = "0.0.0"
 
-# Replace with your actual repository name
-REPO_NAME = "Research_Deployment_Practices"
-AUTHOR_USER_NAME = 'Govindkv'
+
+REPO_NAME="Research_Deployment_Practices"
+AUTHOR_USER_NAME ="Govindkv"
 SRC_REPO = "ccdp"
-AUTHOR_EMAIL = 'Govind26663355@gmail.com'
+AUTHOR_EMAIL="Govind26663355@gmail.com"
 
 setuptools.setup(
-    name=SRC_REPO,
+    name=REPO_NAME,,
     version=__version__,
-    author=AUTHOR_USER_NAME,
+    author= AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small Python package for Predicting Credit Card Default.",
     long_description=long_description,
@@ -25,11 +26,5 @@ setuptools.setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
     },
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    packages=setuptools.find_packages(where="src")
 )
